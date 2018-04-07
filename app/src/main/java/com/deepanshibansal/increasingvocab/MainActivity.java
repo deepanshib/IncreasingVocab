ea79968058d8e16cce0c683fef485b75ef0d8674
deepanshib <deepanshibansal@ymail.com>
2018-04-07 13:21:21 +0530

    designing left

--- app/src/main/java/com/deepanshibansal/increasingvocab/MainActivity.java
+++ app/src/main/java/com/deepanshibansal/increasingvocab/MainActivity.java
@@ -1,14 +1,9 @@
 package com.deepanshibansal.increasingvocab;
 
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -20,19 +15,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
 public class MainActivity extends AppCompatActivity {
 RecyclerView recyclerView;
 RecyclerView.LayoutManager layoutManager;
